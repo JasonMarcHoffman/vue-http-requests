f8b2121c12a5ea611566c4c4eb63ab8bf857a697
JasonMarcHoffman <jasonmarchoffman@gmail.com>
2021-02-19 06:28:54 +1000

    Loading data when component is mounted

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -34,24 +34,29 @@ export default {
     loadExperiences() {
       let vm = this;
       fetch('https://vue-http-demo-4a081-default-rtdb.firebaseio.com/surveys.json')
-      .then(function(response) {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(function(data) {
-        // looping through the data received and generating an object for every id in data
-        const results = [];
-        for (const id in data) {
-          results.push({ 
-            id: id, 
-            name: data[id].name, 
-            rating: data[id].rating
-          });
-        }
-        vm.results = results;
-      })
-    }
+        .then(function(response) {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(function(data) {
+          // looping through the data received and generating an object for every id in data
+          const results = [];
+          for (const id in data) {
+            results.unshift({ 
+              id: id, 
+              name: data[id].name, 
+              rating: data[id].rating
+            });
+          }
+          vm.results = results;
+        })
+      }
+  },
+  // loading the existing experiences so we don't have to click the button
+  mounted() {
+    //  NOTE: we need to run the method with () not just this.loadExperiences
+    this.loadExperiences();
   }
 };
 </script>
